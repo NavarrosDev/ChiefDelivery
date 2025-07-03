76816c232dd0d770f038a574489cd7c7e66dda21
NavarrosDev <gabrielnavarro.dev@gmail.com>
2025-07-03 19:48:56 -0300

    Initial Commit

---
+++ ChiefDelivery/ChiefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChiefDeliveryApp.swift
+//  ChiefDelivery
+//
+//  Created by Gabriel Navarro on 03/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ChiefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
