b7404321bb03d3408c351b44e57e1ff7f8e3540c
NavarrosDev <gabrielnavarro.dev@gmail.com>
2025-07-06 11:35:58 -0300

    Finished project =)

---
+++ ChiefDelivery/CarouselView/CarouselTabView.swift
@@ -0,0 +1,18 @@
+//
+//  CarouselTabView.swift
+//  ChiefDelivery
+//
+//  Created by Gabriel Navarro on 05/07/25.
+//
+
+import SwiftUI
+
+struct CarouselTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarouselTabView()
+}
---
+++ ChiefDelivery/GridView/OrderTypeGridView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderTypeGridView.swift
+//  ChiefDelivery
+//
+//  Created by Gabriel Navarro on 05/07/25.
+//
+
+import SwiftUI
+
+struct OrderTypeGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderTypeGridView()
+}
