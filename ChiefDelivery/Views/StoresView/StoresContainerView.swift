3bc4a9414e272f24b5fa8e9746d72f880d0d7d85
NavarrosDev <gabrielnavarro.dev@gmail.com>
2025-07-07 20:37:42 -0300

    I implemented filters in the store list

--- ChiefDelivery/Views/StoresView/StoresContainerView.swift
+++ ChiefDelivery/Views/StoresView/StoresContainerView.swift
@@ -9,13 +9,76 @@ import SwiftUI
 
 struct StoresContainerView: View {
     let title: String = "Lojas"
+    @State private var ratingFilter: Int = 0
+    @State private var distance: Range<Double> = 0..<50
+    
+    var filteredStores: [StoreType] {
+        return storesMock.filter { store in
+            store.stars >= ratingFilter && distance.contains(store.distance)
+        }
+    }
+    
     var body: some View {
         VStack(alignment: .leading, spacing:  10) {
-            Text(title)
-                .font(.headline)
+            HStack {
+                Text(title)
+                    .font(.headline)
+                
+                Spacer()
+                
+                Menu("Estrelas") {
+                    
+                    Button {
+                        ratingFilter = 0
+                    } label: {
+                        Text("Limpar filtro")
+                    }
+                    
+                    Divider()
+                    
+                    ForEach(1...5, id: \.self) { i in
+                        Button {
+                            ratingFilter = i
+                        } label: {
+                            Text("\(i) \(i == 1 ? "estrela" : "estrelas") ou mais")
+                        }
+                    }
+                }
+                .foregroundColor(.black)
+                
+                Menu("Distancia") {
+                    
+                    Button {
+                        distance = 0..<50
+                    } label: {
+                        Text("Limpar filtro")
+                    }
+                    
+                    Divider()
+                    
+                    ForEach(Array(stride(from: 0, through: 20, by: 5)), id: \.self) { i in
+                        Button {
+                            distance = i..<(i + 6)
+                        } label: {
+                            Text("De \(Int(i)) até \(Int(i + 5)) km")
+                        }
+                    }
+                }
+                .foregroundColor(.black)
+            }
             
             VStack(alignment: .leading, spacing: 30) {
-                ForEach(storesMock) { store in
+                
+                if filteredStores.isEmpty {
+                    Text("Nenhum resultado encontrado.")
+                        .font(.title2)
+                        .bold()
+                        .foregroundColor(Color("ColorRed"))
+                        .padding(.vertical, 32)
+                        .frame(maxWidth: .infinity)
+                }
+                
+                ForEach(filteredStores) { store in
                     NavigationLink {
                         StoreDetailsView(stores: store)
                     } label: {
