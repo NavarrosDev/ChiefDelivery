ab89829c48839adf0e3fc75234eb694765fb9dcc
NavarrosDev <gabrielnavarro.dev@gmail.com>
2025-07-09 22:46:06 -0300

    Implementing last animation in tabview

--- ChiefDelivery/Views/CarouselView/CarouselTabView.swift
+++ ChiefDelivery/Views/CarouselView/CarouselTabView.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct CarouselTabView: View {
+    @State var currentIndex: Int = 1
+    
     var body: some View {
-        TabView {
+        TabView(selection: $currentIndex) {
             ForEach(carouselMock) { order in
                 CarouselItemView(carousel: order)
+                    .tag(order.id)
             }
         }
         .frame(height: 180)
         .tabViewStyle(.page(indexDisplayMode: .always))
+        .onAppear {
+            Timer
+                .scheduledTimer(withTimeInterval: 3,repeats: true) { _ in
+                    withAnimation(.easeInOut(duration: 1)) {
+                      if currentIndex > carouselMock.count {
+                          currentIndex = 1
+                      }
+                      currentIndex += 1
+                    }
+                }
+        }
     }
 }
 
