5ad3b684d875ebf59559ce99a31ebd17ebdaa046
NavarrosDev <gabrielnavarro.dev@gmail.com>
2025-07-09 22:28:38 -0300

    implementing transition button for ContentView

--- ChiefDelivery/HomeView/HomeView.swift
+++ ChiefDelivery/HomeView/HomeView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct HomeView: View {
     @State private var isAnimating: Bool = false
     @State private var imageOffSet: CGSize = .zero
+    @State private var buttonOffSet: CGFloat = 0
+    @State private var showSecondScreen: Bool = false
+    
+    let buttonHeigth: CGFloat = 80
     
     var body: some View {
         GeometryReader { geometry in
@@ -36,7 +40,7 @@ struct HomeView: View {
                 
                 VStack {
                     Text("Chief Delivery")
-                        .font(.system(size: 40))
+                        .font(.system(size: 48))
                         .fontWeight(.heavy)
                         .foregroundColor(Color("ColorRed"))
                         .opacity(isAnimating ? 1 : 0)
@@ -69,12 +73,77 @@ struct HomeView: View {
                                     }
                                 })
                         )
+                    
+                    ZStack {
+                        Capsule()
+                            .fill(Color("ColorRed").opacity(0.2))
+                        Capsule()
+                            .fill(Color("ColorRed").opacity(0.2))
+                            .padding(8)
+                        
+                        Text("Descruba mais")
+                            .font(.title2)
+                            .bold()
+                            .foregroundColor(Color("ColorRedDark"))
+                            .offset(x: 20)
+                        
+                        HStack {
+                            Capsule()
+                                .fill(Color("ColorRed"))
+                                .frame(width: buttonOffSet + buttonHeigth)
+                            
+                            Spacer()
+                        }
+                        
+                        HStack {
+                            ZStack {
+                                Circle()
+                                    .fill(Color("ColorRed"))
+                                Circle()
+                                    .fill(Color("ColorRedDark"))
+                                    .padding(8)
+                                Image(systemName: "chevron.right.2")
+                                    .font(.system(size: 24))
+                                    .bold()
+                                    .foregroundColor(.white)
+                            }
+                            Spacer()
+                        }
+                        .offset(x: buttonOffSet)
+                        .gesture(
+                            DragGesture()
+                                .onChanged { gesture in
+                                    if gesture.translation.width >= 0 &&
+                                        buttonOffSet <= (geometry.size.width - 60 - buttonHeigth)
+                                    {
+                                        withAnimation(.easeInOut(duration: 0.25)) {
+                                            buttonOffSet = gesture.translation.width
+                                        }
+                                    } else {}
+                                    
+                                    
+                                }
+                                .onEnded({ gesture in
+                                    if buttonOffSet > (geometry.size.width - 60) / 2 {
+                                        showSecondScreen = true
+                                    } else {
+                                        withAnimation(.easeInOut(duration: 0.25)) {
+                                            buttonOffSet = 0
+                                        }
+                                    }
+                                })
+                        )
+                    }
+                    .frame(width: geometry.size.width - 50, height: buttonHeigth)
                 }
                 .onAppear {
                     withAnimation(.easeInOut(duration: 1)) {
                         isAnimating = true
                     }
                     
+                }
+                .fullScreenCover(isPresented: $showSecondScreen) {
+                    ContentView()
                 }
             }
         }
